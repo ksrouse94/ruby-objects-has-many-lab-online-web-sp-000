9b61db5179ff6c48c3973f4c20a2d419717e7170
Kimberly Rouse <ksrouse94@gmail.com>
2019-09-18 22:32:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,8 @@ class Artist
   end
   
   def songs 
-    Song.all.select {|song| song.artist == self}
+    songs = Song.all.select {|song| song.artist == self}
+    
   end 
   
   def add_song(song)
